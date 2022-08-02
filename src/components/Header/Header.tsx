6e616e3780229bcd0755e3ce7006b79b9009f82b
Nikita Abyshkin <ride1122@mail.ru>
2022-08-02 18:41:54 +0700

    Make ui-kit folder

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Logo } from '../../ui-kit';
 import styles from './Header.module.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      Header
+      <div>
+        <Logo />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
