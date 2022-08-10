6e1059f07dfa7905791ce9aa23cc98af109a1991
Nikita Abyshkin <ride1122@mail.ru>
2022-08-10 15:20:54 +0700

    Added programitem

--- src/components/Program/Program.tsx
+++ src/components/Program/Program.tsx
@@ -5,38 +5,69 @@ import styles from './Program.module.scss';
 const items = [
   {
     id: 0,
+    icon: '',
     title: 'Разработка мобильных приложений',
     text: 'Разработчик мобильных приложений создаёт приложения, которыми люди ежедневно пользуются на смартфонах, умных часах и планшетах.',
+    program: 'Курс',
+    category: 'Робототехника',
     duration: 24,
+    age: 'child',
     level: 'light',
     type: 'any',
-    icon: '',
-    sale: '10',
-    color: '#C7EEFF',
+    sale: 10,
   },
   {
     id: 1,
+    icon: '',
     title: 'Разработка мобильных приложений',
     text: 'Разработчик мобильных приложений создаёт приложения, которыми люди ежедневно пользуются на смартфонах, умных часах и планшетах.',
+    program: 'Интенсивный курс',
+    category: 'Английский язык',
     duration: 12,
+    age: 'teenagers',
     level: 'light',
     type: 'any',
+    sale: 10,
+  },
+  {
+    id: 2,
     icon: '',
-    sale: '10',
-    color: '#FFCE94',
+    title: 'Разработка мобильных приложений',
+    text: 'Разработчик мобильных приложений создаёт приложения, которыми люди ежедневно пользуются на смартфонах, умных часах и планшетах.',
+    program: 'Курс',
+    category: 'Шахматы',
+    duration: 12,
+    age: 'adults',
+    level: 'light',
+    type: 'any',
+    sale: 10,
   },
 ];
 
-const Program = () => {
+const colors = [
+  '#C7EEFF',
+  '#FFCE94',
+  '#FFE38E',
+  '#DDDDFF',
+  '#B8EFCF',
+  '#C7EEFF',
+  '#C5DDFF',
+  '#FFEEF6',
+];
+
+const Program: React.FC = () => {
   return (
     <div className={styles.container}>
-      {items.map((obj) => (
+      {items.map((obj, index: number) => (
         <ProgramItem
           key={obj.id}
           title={obj.title}
           text={obj.text}
           duration={obj.duration}
-          color={obj.color}
+          icon={obj.icon}
+          program={obj.program}
+          category={obj.category}
+          color={colors[index]}
         />
       ))}
     </div>
--- src/components/ProgramItem/ProgramItem.tsx
+++ src/components/ProgramItem/ProgramItem.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import styles from './ProgramItem.module.scss';
+import defaultIcon from '../../assets/program-icon.svg';
 
-type ProgramItem = {
+type TProgramItem = {
   title: string;
   text: string;
   duration: number;
+  icon: string;
   color: string;
+  program: string;
+  category: string;
 };
 
-const ProgramItem: React.FC<ProgramItem> = ({ title, text, duration, color }) => {
+const ProgramItem: React.FC<TProgramItem> = ({
+  title,
+  text,
+  duration,
+  color,
+  icon,
+  program,
+  category,
+}) => {
   return (
     <div className={styles.container} style={{ backgroundColor: color }}>
-      <h3>{title}</h3>
+      <div className={styles.header}>
+        <span>{program}</span>
+        <span>{category}</span>
+      </div>
+      <div className={styles.title}>
+        <img src={icon || defaultIcon} alt="" />
+        <h3>«{title}»</h3>
+      </div>
       <p>{text}</p>
-      <span>{duration}</span>
+      <span className={styles.duration}>Длительность: {duration} месяцев</span>
     </div>
   );
 };
